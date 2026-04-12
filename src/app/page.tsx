0ced9276611a89003db1168a2376995f33149c94
kushagrasaxena28 <kushagra.saxena2015.ks@gmail.com>
2026-04-12 23:51:09 +0530

    feat: initialize project with Shadcn UI component library and Prisma database setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
+import { cn } from "@/lib/utils"
+import { Button } from "@/components/ui/button"
 
 
 export default function Home() {
   return (
-    <div className="text-red-500">
-      Hello World
+    <div className={cn("text-red-500", "text-2xl")}>
+      <Button>Hello World</Button>
     </div>
   );
 }
